08419b40137c31d25715a04149f02543ea0349bf
Huzaifa <30310865+hm-huzaifa@users.noreply.github.com>
2024-01-24 16:36:53 +0500

    Delete src/environments/environment.ts

--- src/environments/environment.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: false,
-  TMDB_AUTH_KEY: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhZDI3ZDlhYjdiNzg4NjhjYmIxNjM4OTQzNmMzNjIwNiIsInN1YiI6IjY1YWZiNTZlYWFkOWMyMDBhZGZlYjM5MiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.t1-RQabQlyQrvgfum45IFWjqpujN_NitoMk-mDoWEXY',
-  GOOGLE_CLIENT_ID: '546786901789-h0ved8bvpqqhoehou4pit0bfncfrq6ft.apps.googleusercontent.com'
-};
