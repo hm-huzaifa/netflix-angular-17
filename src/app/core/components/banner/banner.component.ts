c92e5ce891696d05be9324ab2ed18c66707d4641
hm-huzaifa <mhuzaifa150@gmail.com>
2024-01-24 18:22:35 +0500

    minor bug fixes x2

--- src/app/core/components/banner/banner.component.ts
+++ src/app/core/components/banner/banner.component.ts
@@ -1,25 +1,33 @@
-import {Component, inject, Input, OnChanges, SimpleChanges} from '@angular/core';
-import {DomSanitizer} from "@angular/platform-browser";
+import {
+  Component,
+  inject,
+  Input,
+  OnChanges,
+  SimpleChanges,
+} from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-banner',
   standalone: true,
   imports: [],
   templateUrl: './banner.component.html',
-  styleUrl: './banner.component.scss'
+  styleUrl: './banner.component.scss',
 })
-export class BannerComponent implements OnChanges{
+export class BannerComponent implements OnChanges {
   @Input() bannerTitle = '';
-  @Input() bannerOverview = ''
-  @Input() key = ''
+  @Input() bannerOverview = '';
+  @Input() key = '';
   private sanitizer = inject(DomSanitizer);
-  videoUrl = this.sanitizer.bypassSecurityTrustResourceUrl(`https://www.youtube.com/embed/${this.key}?autoplay=1&mute=1&loop=1&controls=0`)
+  videoUrl = this.sanitizer.bypassSecurityTrustResourceUrl(
+    `https://www.youtube.com/embed/${this.key}?autoplay=1&mute=1&loop=1&controls=0`
+  );
 
   ngOnChanges(changes: SimpleChanges): void {
-    if (changes['key']){
-      this.videoUrl = this.sanitizer.bypassSecurityTrustResourceUrl(`https://www.youtube.com/embed/${this.key}?autoplay=1&mute=1&loop=1&controls=0`)
-
+    if (changes['key']) {
+      this.videoUrl = this.sanitizer.bypassSecurityTrustResourceUrl(
+        `https://www.youtube.com/embed/${this.key}?autoplay=1&mute=1&loop=1&controls=0`
+      );
     }
   }
-
 }
